fbc0ab4c024b5c19ae12e97aa8e634d956c9ca1f
NataliaP <natalia.prosinska@gmail.com>
2015-04-21 22:21:45 +0200

    Create ps1a.py

---
+++ ps1a.py
@@ -0,0 +1,34 @@
+# Problem Set 1 
+# Name: Natalia Prósińska
+# Collaborators: Małgorzata Metryka 
+# Time Spent: 3:00
+
+initBal = float(input('Enter the outstanding balance on your credit card: '))
+annualRate = float(input('What is your annual percentage rate (as a decimal, i.e. 18% is .18): '))
+minPayRate = float(input('Enter your minimum monthly payment rate (as a decimal, i.e. 2% is .02): '))
+
+monthlyRate = annualRate/12
+
+numMonths = 1
+totalPaid = 0
+
+while numMonths <= 12:
+
+   minPayment = round(minPayRate * initBal,2)
+   totalPaid += minPayment
+
+   interestPaid = round(monthlyRate * initBal,2)
+
+   principalPaid = minPayment - interestPaid
+
+   initBal -= principalPaid
+
+   print ('Month:, ', numMonths)
+   print ('Minimum monthly payment amount: $', minPayment)
+   print ('Remaining balance: $', initBal)
+
+   numMonths += 1
+
+print ('RESULT')
+print ('Total amount paid: $', totalPaid)
+print ('Remaining balance: $', initBal)
