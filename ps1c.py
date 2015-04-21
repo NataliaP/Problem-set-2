57be8afe87b2dffb2f7fd2c0aff9d1eeec91421e
NataliaP <natalia.prosinska@gmail.com>
2015-04-21 22:28:21 +0200

    Create ps1c.py

---
+++ ps1c.py
@@ -0,0 +1,44 @@
+# Problem Set 2 
+# Name: Natalia Prósińska 
+# Collaborators: Małgorzata Metryka
+# Time Spent: 3:00 
+
+Initialbal = float(input("What is your outstanding balance on your credit card: "))
+interestrate = float(input("What is your annual percentage rate (as a decimal, i.e. 18% is .18): "))
+
+bal = Initialbal
+lowpay = bal/12
+highpay = (bal*(1+(interestrate/12))**12)/12
+
+while True:
+   bal = Initialbal
+   monthlypay = (lowpay + highpay)/2
+
+   for month in range(1,13):
+       interest = round(bal*interestrate/12, 2)
+       bal += interest - monthlypay
+       if bal <= 0:
+           break
+
+   if (highpay - lowpay < 0.003):
+
+       print ("RESULT")
+
+       monthlypay = round(monthlypay + 0.002999, 2)
+       print ("Monthly payment to pay off debt in 1 year: $", round(monthlypay,2))
+
+       bal = Initialbal
+       for month in range(1,13):
+           interest = round(bal*interestrate/12, 2)
+           bal += interest - monthlypay
+           if bal <= 0:
+               break
+       print ("Number of months needed:", month)
+       print ("Balance: $", round(bal,2))
+       break
+   elif bal < 0:
+
+       highpay = monthlypay
+   else:
+
+       lowpay = monthlypay
